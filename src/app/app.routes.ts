c3f76b5934b97950a7e3acc13a76ebf82d8640ea
Clau <clau.quevv@gmail.com>
2025-11-25 19:19:21 -0500

    Actualización menor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
+import { SimpsonDetailPage } from './features/simpsons/pages/simpson-detail-page/simpson-detail-page';
 import { Routes } from '@angular/router';
 import { EstilosPage } from './features/estilos-page/estilos-page';
 import { SignalBoxComponent } from './features/SignalBoxComponent/SignalBoxComponent';
 import { DaisyuiPage } from './features/daisyui-page/daisyui-page';
 import { SimpsonsPageComponent } from './features/simpsons/pages/simpsons-page/simpsons-page';
-import { SimpsonDetailPage } from './features/simpsons/pages/simpson-detail-page/simpson-detail-page';
+
 
 export const routes: Routes = [
   {
@@ -24,6 +25,9 @@ export const routes: Routes = [
 },
 {
   path: 'simpsons/:id',
-  component: SimpsonDetailPage,
+  loadComponent: () =>
+    import('./features/simpsons/pages/simpson-detail-page/simpson-detail-page')
+      .then(m => m.SimpsonDetailPage),
+  data: { renderMode: 'client' } 
 }
 ];
